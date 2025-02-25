165df1bb4efc662ef72e324ea7ccb01263c408cd
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-02-25 12:55:43 +0100

    edit footer text

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -3,10 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-light text-center py-3">
-      <p>
-        &copy; {new Date().getFullYear()} My E-commerce Store. All Rights
-        Reserved.
-      </p>
+      <p>&copy; {new Date().getFullYear()} Bernhard Scheucher</p>
     </footer>
   );
 }
